49582efd521954d008f99fcb074cb83f9adf6fa2
Cornelia Schulz <firzhugh@gmail.com>
2016-10-02 22:18:05 +1300

    Added Venue List Component

---
+++ src/components/application/homepage/VenueList.js
@@ -0,0 +1,23 @@
+'use strict';
+
+import React from 'react';
+
+require('styles/application/homepage/Search.css');
+
+class VenueList extends React.Component {
+  render() {
+    return (
+      <div className="venue-list">
+        Please edit src/components/application/homepage//SearchComponent.js to update this component!
+      </div>
+    );
+  }
+}
+
+VenueList.displayName = 'ApplicationHomepageVenueList';
+
+// Uncomment properties you need
+// VenueList.propTypes = {};
+// VenueList.defaultProps = {};
+
+export default VenueList;
