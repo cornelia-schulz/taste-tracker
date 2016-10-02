ecc7de4970995f7e0c9d159ccbda63a21c74c58c
Dave Walker <walkerd@fphcare.com>
2016-10-02 21:28:24 +1300

    Initial commit

---
+++ src/components/Main.js
@@ -0,0 +1,22 @@
+require('normalize.css/normalize.css');
+require('styles/App.css');
+import Search from './application/homepage/SearchComponent';
+
+import React from 'react';
+
+let yeomanImage = require('../images/yeoman.png');
+
+class AppComponent extends React.Component {
+  render() {
+    return (
+      <div className="index">
+        <Search/>
+      </div>
+    );
+  }
+}
+
+AppComponent.defaultProps = {
+};
+
+export default AppComponent;
---
+++ src/components/application/homepage/SearchComponent.js
@@ -0,0 +1,23 @@
+'use strict';
+
+import React from 'react';
+
+require('styles/application/homepage/Search.css');
+
+class SearchComponent extends React.Component {
+  render() {
+    return (
+      <div className="search-component">
+        Please edit src/components/application/homepage//SearchComponent.js to update this component!
+      </div>
+    );
+  }
+}
+
+SearchComponent.displayName = 'ApplicationHomepageSearchComponent';
+
+// Uncomment properties you need
+// SearchComponent.propTypes = {};
+// SearchComponent.defaultProps = {};
+
+export default SearchComponent;
